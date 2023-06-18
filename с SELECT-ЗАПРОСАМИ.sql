7b273f9127d00b323cac5d06806ebe0e1ff359fb
Nahodka230 <nahodka230@gmail.com>
2023-06-18 13:21:41 +0500

    Refinement

--- с SELECT-ЗАПРОСАМИ.sql
+++ с SELECT-ЗАПРОСАМИ.sql
@@ -11,25 +11,29 @@ SELECT name FROM singer
 WHERE name NOT LIKE '% %';
 
 SELECT name FROM track 
-WHERE name LIKE '%my%';
+WHERE string_to_array(lower(name), ' ') && ARRAY['my'];
 
 SELECT name, COUNT(singer_id) singer_q FROM genre g
 JOIN singer_genre sg ON g.genre_id = sg.genre_id
 GROUP BY g.name;
 
-SELECT a.prod_year, COUNT(t.album_id) track_q FROM album a 
-JOIN track t ON a.album_id = t.album_id
-WHERE a.prod_year BETWEEN 2019 AND 2020
-GROUP BY a.prod_year;
+SELECT COUNT(track_id) FROM track t
+JOIN album a ON t.album_id = a.album_id
+WHERE a.prod_year BETWEEN 2019 and 2020; 
 
 SELECT a.name, AVG(t.duration) avg_duration FROM album a
 JOIN track t ON a.album_id = t.album_id
 GROUP BY a.name;
 
-SELECT s.name singer_name, a.name album_name, a.prod_year FROM singer s
-JOIN singer_album sa ON s.singer_id = sa.singer_id
-JOIN album a ON sa.album_id = a.album_id
-WHERE a.prod_year != 2020;
+SELECT s.name 
+FROM singer s  
+WHERE s.name NOT IN ( 
+    SELECT sr.name 
+    FROM singer sr 
+    JOIN singer_album sa ON sr.singer_id = sa.singer_id
+    JOIN album a ON sa.album_id = a.album_id
+    WHERE a.prod_year = 2020
+);
 
 SELECT s.name singer_name, c.name collection_name FROM singer s
 JOIN singer_album sa ON s.singer_id = sa.singer_id 
@@ -38,3 +42,4 @@ JOIN track t ON a.album_id = t.album_id
 JOIN collection_track ct ON t.track_id = ct.track_id 
 JOIN collection c ON c.collection_id =ct.collection_id 
 WHERE s.name = 'Gina Gina';
+
